953c20aa0e9e294a864ba4f2cc2cc8b74e5f4570
Sergio Ojeda <sojeda@inameh.gob.ve>
2016-05-25 11:19:21 -0400

    CRUD

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,49 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+use App\User;
+use Faker\Factory as Faker;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+# CRUD
+Route::get('create', function() {
+
+	$faker = Faker::create();
+
+    $user = User::create([
+    	'name' => $faker->name,
+    	'email' => $faker->email,
+    	'password' => bcrypt('123456'),
+    	'gender' => $faker->randomElement(['f','m']),
+    	'biography' => $faker->text(240),
+    ]);
+
+    return $user;
+});
+
+Route::get('read/{id}', function($id) {
+    $user = User::find($id);
+    return $user;
+});
+
+Route::get('update/{id}', function($id) {
+
+	$faker = Faker::create();
+    $user = User::find($id);
+    $user->name = $faker->name;
+    $user->gender = $faker->randomElement(['f','m']);
+    $user->biography = $faker->text(240);
+    $user->save();
+
+    return $user;
+
+});
+
+Route::get('delete/{id}', function($id) {
+    $user = User::find($id);
+    $user->delete();
+    return 'Usuario con id '. $id. ' eliminado';
 });
